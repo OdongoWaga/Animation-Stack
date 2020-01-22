653098a8fe527addefea6281db0ddc1dbfd4050b
Waga <wagaodongo@gmail.com>
2020-01-22 11:25:10 +0300

    Animation Order

--- Animation Stack/ContentView.swift
+++ Animation Stack/ContentView.swift
@@ -9,8 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enabled = false
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            self.enabled.toggle()
+            
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(.default)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60: 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+        
     }
 }
 
